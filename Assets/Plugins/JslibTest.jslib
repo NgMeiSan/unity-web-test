a3c8393d2b088732f216646d7afcc2c458b313dc
NgMeiSan <meisan.ng@hashcodestudio.com>
2024-10-23 09:56:04 +0800

    Add jslib

---
+++ Assets/Plugins/JslibTest.jslib
@@ -0,0 +1,68 @@
+
+mergeInto(LibraryManager.library,
+{
+    ImportTelegramlibrary: function(callback)
+    {
+        var script = document.createElement("script");
+        script.src = "https://telegram.org/js/telegram-web-app.js";
+        document.head.appendChild(script);
+
+        script.onload = function ()
+        {
+            console.log("TELEGRAM SCRIPT LOADED");
+            {{{ makeDynCall('v', 'callback') }}} ();
+        };
+    },
+
+    Alert: function()
+    {
+        window.alert("Unity to JS Alert!");
+    },
+
+    DebugTest: function()
+    {
+        console.log(window.Telegram);
+    },
+
+    GetUserId: function()
+    {
+        var retunStr = "";
+
+        var tg = window.Telegram.WebApp;
+        var user = tg.initDataUnsafe.user;
+
+        if (user) {
+            var userId = user.id;
+            console.log('User ID:', userId);
+            retunStr = userId;
+        } else {
+            console.log('No user data available');
+            return;
+        }
+
+        var bufferSize = lengthBytesUTF8(retunStr) + 1
+        var buffer = _malloc(bufferSize);
+
+        stringToUTF8(retunStr, buffer, bufferSize);
+
+        return buffer;
+    },
+
+    GetSampleData: function()
+    {
+        var userData = {
+            id: 12345,
+            username: "example_user",
+            is_bot: false
+        };
+
+        var jsonString = JSON.stringify(userData);
+
+        var bufferSize = lengthBytesUTF8(jsonString) + 1
+        var buffer = _malloc(bufferSize);
+
+        stringToUTF8(jsonString, buffer, bufferSize);
+
+        return buffer;
+    }
+})
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,10 +1,108 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
+using System;
+using AOT;
 
 public class Test : MonoBehaviour
 {
-    public void PrintMessage(string message) {
+    #if UNITY_WEBGL
+    #region  Variables~
+
+    #region DllImport
+
+    [DllImport("__Internal")]
+    public static extern void ImportTelegramlibrary(Action onLoadedCallback);
+
+    [DllImport("__Internal")]
+    private static extern void Alert();
+
+    [DllImport("__Internal")]
+    private static extern void DebugTest();
+
+    [DllImport("__Internal")]
+    private static extern string GetUserId();
+
+    [DllImport("__Internal")]
+    private static extern string GetSampleData();
+
+    // [DllImport("__Internal")]
+    // public static extern void JsSetTimeout(string message, int timeout, Action<string> action);
+
+    #endregion
+
+    #endregion
+    #endif
+
+    void Start()
+    {
+        #if UNITY_WEBGL
+        LoadTelegramLibrary();
+        #endif
+    }
+
+    #if UNITY_WEBGL
+
+    public static void AlertWeb()
+    {
+        Alert();
+    }
+
+    public static void PrintTelegram()
+    {
+        DebugTest();
+    }
+
+    public static void PrintUserId()
+    {
+        Debug.Log(GetUserId());
+    }
+
+    public static void LoadTelegramLibrary()
+    {
+        [MonoPInvokeCallback(typeof(Action))]
+        static void OnTelegramLibraryLoaded()
+        {
+            Debug.Log("CALLBACK FROM UNITY");
+        }
+
+        ImportTelegramlibrary(OnTelegramLibraryLoaded);
+    }
+
+    [MonoPInvokeCallback(typeof(Action<string>))]
+    public static void CSSharpCallback(string message)
+    {
+        Debug.Log($"C# callback received \"{message}\"");
+    }
+
+    public static void PrintSampleData()
+    {
+        // Call the JavaScript function and get the JSON string
+        string sampleDataJson = GetSampleData();
+
+        // Parse the JSON string (using UnityEngine.JsonUtility or any other JSON parser)
+        SampleData userData = JsonUtility.FromJson<SampleData>(sampleDataJson);
+
+        // Now you can use the userData object in Unity
+        Debug.Log("User ID: " + userData.id);
+        Debug.Log("Username: " + userData.username);
+        Debug.Log("Is Bot: " + userData.is_bot);
+    }
+
+    #endif
+    
+    public void PrintMessage(string message)
+    {
         Debug.Log(message);
     }
 }
+
+#if UNITY_WEBGL
+// Define a class to match the JSON structure
+[System.Serializable]
+public class SampleData
+{
+    public int id;
+    public string username;
+    public bool is_bot;
+}
+#endif
